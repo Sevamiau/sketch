c249bc7652b9be34560d206a1bae7976643205e2
¨Seva <sevakunjadas.bms@gmail.com>
2025-08-05 23:45:25 -0300

    create func creatediv and divs

---
+++ js.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
+const para = document.createElement('p');
+para.textContent = 'soy rojo';
+para.style.color = 'red';
+
+container.appendChild(para);
