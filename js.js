3a4c0c3fb7414ee508758cdf299138c90ced848f
¨Seva <sevakunjadas.bms@gmail.com>
2025-08-07 20:10:36 -0300

    feat: add workin interactive grid

--- js.js
+++ js.js
@@ -1,13 +1,50 @@
-const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+const container = document.querySelector('#container');
+container.style.touchAction = 'none';
 
-container.appendChild(content);
+// console.log(container);
 
-const para = document.createElement('p');
-para.textContent = 'soy rojo';
-para.style.color = 'red';
+let isDrawing = false;
 
-container.appendChild(para);
+for(let i = 0; i <256; i++){
+    const newDiv = document.createElement('div');
+    newDiv.classList.add()
+    // console.log(i+1);
+    // newDiv.appendChild(
+    //     document.createTextNode(i+1)
+    // )
+
+    newDiv.addEventListener('pointerdown', (e) => {
+        console.log(e);
+        isDrawing = true;
+        newDiv.style.backgroundColor = 'pink';
+
+    })
+
+    newDiv.addEventListener('pointermove', (e) => {
+        if(!isDrawing) return;
+        isDrawing = true;
+        newDiv.style.backgroundColor = 'pink';
+
+    })
+
+    const stop = (e) => {
+        isDrawing = false;
+        container.releasePointerCapture(e.pointerId);
+    }
+
+    container.addEventListener('pointerup', stop);
+    container.appendChild(newDiv);
+    
+}
+
+const dialog = document.querySelector('dialog');
+
+function showDialog() {
+    dialog.showModal();
+}
+
+function closeDialog() {
+    dialog.close(); 
+}
+
