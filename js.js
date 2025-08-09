560de1b12252ed646b320b3f0598aefa32312704
¨Seva <sevakunjadas.bms@gmail.com>
2025-08-08 23:08:30 -0300

    feat: reset function

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#container');
 const resizeButton = document.querySelector('#resizeButton');
+const resetButton = document.querySelector('#resetButton')
 
 let isDrawing = false;
 
@@ -30,42 +31,11 @@ function createGrid(size) {
 
         const stop = (e) => {
             isDrawing = false;
-            // container.realeasePointerCapture(e.pointerId);
         }
 
         container.addEventListener('pointerup', stop);
-        // container.appendChild(squares);
 
     })
-
-
-    // for(let i = 0; i <size * size; i++){
-//     const newDiv = document.createElement('div');
-//     newDiv.classList.add()
- 
-//     newDiv.addEventListener('pointerdown', (e) => {
-//         console.log(e);
-//         isDrawing = true;
-//         newDiv.style.backgroundColor = 'pink';
-
-//     })
-
-//     newDiv.addEventListener('pointermove', (e) => {
-//         if(!isDrawing) return;
-//         isDrawing = true;
-//         newDiv.style.backgroundColor = 'pink';
-
-//     })
-
-//     const stop = (e) => {
-//         isDrawing = false;
-//         container.releasePointerCapture(e.pointerId);
-//     }
-
-//     container.addEventListener('pointerup', stop);
-//     container.appendChild(newDiv);
-// }
-
 
 
 }
@@ -77,71 +47,9 @@ resizeButton.addEventListener('click', () => {
     newSize = parseInt(newSize);
     createGrid(newSize);
     
-});
+})
 
-
-
-
-
-
-
-// const container = document.querySelector('#container');
-// const resizeButton = document.querySelector('#resizeButton');
-
-// let isDrawing = false;
-
-// let size = 16;
-
-// for(let i = 0; i <size * size; i++){
-//     const newDiv = document.createElement('div');
-//     newDiv.classList.add()
- 
-//     newDiv.addEventListener('pointerdown', (e) => {
-//         console.log(e);
-//         isDrawing = true;
-//         newDiv.style.backgroundColor = 'pink';
-
-//     })
-
-//     newDiv.addEventListener('pointermove', (e) => {
-//         if(!isDrawing) return;
-//         isDrawing = true;
-//         newDiv.style.backgroundColor = 'pink';
-
-//     })
-
-//     const stop = (e) => {
-//         isDrawing = false;
-//         container.releasePointerCapture(e.pointerId);
-//     }
-
-//     container.addEventListener('pointerup', stop);
-//     container.appendChild(newDiv);
-// }
-
-
-// resizeButton.addEventListener('click', () => {
-//     // alert('miau');
-//     let newSize = prompt("elegite el numero pal grid amigo (maximo 100):");
-//     newSize = parseInt(newSize);
-//     size = newSize;
-
-// });
-
-// // function resizeGrid(){
-// //     const inputElement = document.getElementById('grid-size');
-// //     alert(inputElement.value);
-
-// // }
-
-
-
-
-// // resizeButton.addEventListener("click", () => {
-// // //   let newSize = prompt("elegite el numero pal grid amigo (maximo 100):");
-// // //   newSize = parseInt(newSize);
-// //     alert('miau');
-
-// // });
-
+resetButton.addEventListener('click', () => {
+    createGrid(16);
+})
 
